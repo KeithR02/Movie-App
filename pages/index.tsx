d2a84c2785bffbc21f1b0d4c1ccb14f1b3c0be8a
Keith Roberts <keith_roberts@stu.indianhills.edu>
2025-10-22 22:41:52 -0500

    Created a basic homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,27 @@
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
+export default function HomePage() {
+    return (
+        <main>
+            <div>
+                <h1>Movie Matcher</h1>
+                <p>Swipe until you find the perfect movie.</p>
+            </div>
 
-const client = generateClient<Schema>();
-
-export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+            <div>
+                <button>
+                    Start swiping
+                </button>
+                    
+                <button>
+                    Explore watchlist
+                </button>
+            </div>
+            
+            <div>
+                <h2>Your watchlist</h2>
+                <button>
+                    Manage
+                </button>
+            </div>
+        </main>
+    );
 }
